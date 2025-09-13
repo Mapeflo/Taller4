798d2adba444094d3ed0506078448556d6734127
mapeflo <mpenarandaf@unicartagena.edu.co>
2025-09-12 19:39:53 -0500

    Avance del taller 4 con ejercicios 1 y 2 sin terminar y 3 por empezar.

---
+++ src/taller4/Coche.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller4;
+
+/**
+ *
+ * @author marya
+ */
+public class Coche {
+  
+    private String marca; 
+    private String modelo;
+    private double velocidadMaxima;
+
+    public Coche(String marca, String modelo, double velocidadMaxima) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+public void acelerar (double incremento) {
+   if (incremento > 0) {
+       velocidadMaxima += incremento;
+       System.out.println("La velocidad maxima se incremento a: " + velocidadMaxima + "k/h");
+    } 
+   else {
+       System.out.println("Incremento no positivo. No hay cambios en velocidad maxima: " + velocidadMaxima + "k/h");
+       } 
+   }
+  }  
+    
+
---
+++ src/taller4/Estudiante.java
@@ -0,0 +1,46 @@
+package taller4;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author marya
+ */
+public class Estudiante {
+    
+    private String nombre;
+    private int edad;
+    private double notaPromedio;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getNotaPromedio() {
+        return notaPromedio;
+    }
+
+    public void setNotaPromedio(double notaPromedio) {
+        this.notaPromedio = notaPromedio;
+    }
+    
+    
+    
+    
+    
+}
